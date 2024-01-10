078d0de9d2a681ae8b37d6edaed0b28135d44d1c
Victor Romanov <gerter666@hotmail.com>
2024-01-10 17:05:18 +0300

    quest patcher cleanup for #6 & #7

--- src-tauri/patcher/src/patch_strategy/quest/mod.rs
+++ src-tauri/patcher/src/patch_strategy/quest/mod.rs
@@ -12,40 +12,40 @@ pub struct  PlayerSpecific {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub(self) struct Primary {
+pub struct Primary {
     #[serde(rename = "Common")]
-    pub(self) common: Option<QuestList>,
+    pub common: Option<QuestList>,
     #[serde(rename = "PlayerSpecific")]
-    pub(self) player_specific: PlayerSpecific
+    pub player_specific: PlayerSpecific
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub(self) struct Secondary {
+pub struct Secondary {
     #[serde(rename = "Common")]
-    pub(self) common: Option<QuestList>,
+    pub common: Option<QuestList>,
     #[serde(rename = "PlayerSpecific")]
-    pub(self) player_specific: PlayerSpecific
+    pub player_specific: PlayerSpecific
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename = "Objectives")]
-pub(self) struct ObjectivesInfo {
+pub struct ObjectivesInfo {
     #[serde(rename = "Primary")]
-    pub(self) primary: Primary,
+    pub primary: Primary,
     #[serde(rename = "Secondary")]
-    pub(self) secondary: Secondary
+    pub secondary: Secondary
 }
 
 impl Homm5Type for ObjectivesInfo {
 }
 
 /// Provides information that can be used across different patches in QuestPatchesGroup
-pub struct QuestInfoProvider<'a> {
-    primary_quests: &'a Vec<Quest>,
-    secondary_quests: &'a Vec<Quest>
+pub struct QuestInfoProvider {
+    primary_quests: Vec<Quest>,
+    secondary_quests: Vec<Quest>
 }
 
-impl<'a> QuestInfoProvider<'a> {
+impl QuestInfoProvider {
     pub fn new(config: &PathBuf) -> Self {
         let primary_de: Vec<Quest> = quick_xml::de::from_str(
             &std::fs::read_to_string(config.join("primary_quests.xml")).unwrap()
@@ -54,15 +54,15 @@ impl<'a> QuestInfoProvider<'a> {
             &std::fs::read_to_string(config.join("secondary_quests.xml")).unwrap()
         ).unwrap();
         QuestInfoProvider { 
-            primary_quests: &primary_de, 
-            secondary_quests: &secondary_de 
+            primary_quests: primary_de, 
+            secondary_quests: secondary_de
         }
     }
 }
 
 /// TownPatchesGroup combines all necessary patches game objectives.
 pub struct QuestPatchesGroup<'a> {
-    patches: Vec<&'a dyn PatchModifyable<Modifyable = ObjectivesInfo>>
+    patches: Vec<&'a mut dyn PatchModifyable<Modifyable = ObjectivesInfo>>
 }
 
 impl<'a> QuestPatchesGroup<'a>  {
@@ -71,20 +71,22 @@ impl<'a> QuestPatchesGroup<'a>  {
             patches: vec![] 
         }
     }
+
+    pub fn with_modifyable(mut self, patch: &'a mut dyn PatchModifyable<Modifyable = ObjectivesInfo>) -> Self {
+        self.patches.push(patch);
+        self
+    }
 }
 
 impl<'a> PatchGroup for QuestPatchesGroup<'a>  {
-    fn with_modifyable(&mut self, patch: &dyn super::PatchModifyable<Modifyable = impl Homm5Type>) -> &mut Self {
-        self.patches.push(patch)
-    }
-
-    fn run(&mut self, text: &String) {
-        let quests_de: Result<ObjectivesInfo, quick_xml::DeError> = quick_xml::de::from_str(&text);
+    fn run(&mut self, text: &String, writer: &mut quick_xml::Writer<&mut Vec<u8>>) {
+        let quests_de: Result<ObjectivesInfo, quick_xml::DeError> = quick_xml::de::from_str(&format!("<Objectives>{}</Objectives>", text));
         match quests_de {
-            Ok(mut quests_de) => {    
-                for patch in self.patches {
-                    patch.try_modify(&mut quests_de);
+            Ok(mut quests) => {    
+                for patch in self.patches.iter_mut() {
+                    patch.try_modify(&mut quests);
                 }
+                writer.write_serializable("Objectives", &quests).unwrap();
             },
             Err(e) => {
                 println!("Error deserializing quests: {}", e.to_string())
--- src-tauri/patcher/src/patch_strategy/quest/modifiers.rs
+++ src-tauri/patcher/src/patch_strategy/quest/modifiers.rs
@@ -5,11 +5,11 @@ use super::{QuestInfoProvider, ObjectivesInfo};
 
 /// Adds HIDDEN quest which is used to trigger map script initialization.
 pub struct MapInitQuestCreator<'a> {
-    quest_info_provider: &'a QuestInfoProvider<'a>
+    quest_info_provider: &'a QuestInfoProvider
 }
 
 impl<'a> MapInitQuestCreator<'a>  {
-    pub fn new(qip: &QuestInfoProvider) -> Self {
+    pub fn new(qip: &'a QuestInfoProvider) -> Self {
         MapInitQuestCreator { 
             quest_info_provider: qip
         }
@@ -20,13 +20,18 @@ impl<'a> PatchModifyable for MapInitQuestCreator<'a> {
     type Modifyable = ObjectivesInfo;
 
     fn try_modify(&mut self, object: &mut Self::Modifyable) {
-        match object.primary.common.unwrap().objectives {
-            Some(mut objectives) => {
-                objectives.items.unwrap().append(&mut self.quest_info_provider.secondary_quests);
+        match object.secondary.common.as_mut().unwrap().objectives.as_mut() {
+            Some(objectives) => {
+                for quest in self.quest_info_provider.secondary_quests.iter() {
+                    match objectives.items.as_mut() {
+                        Some(items) => items.push(quest.clone()),
+                        None => objectives.items = Some(vec![quest.clone()])
+                    }
+                }
             },
             None => {
-                object.primary.common.unwrap().objectives = Some(Objectives { 
-                    items: Some(self.quest_info_provider.secondary_quests) 
+                object.secondary.common.as_mut().unwrap().objectives = Some(Objectives { 
+                    items: Some(self.quest_info_provider.secondary_quests.clone()) 
                 })
             }
         }
@@ -35,11 +40,11 @@ impl<'a> PatchModifyable for MapInitQuestCreator<'a> {
 
 /// Adds quests-descriptions for map modes
 pub struct MapModesQuestCreator<'a> {
-    quest_info_provider: &'a QuestInfoProvider<'a>
+    quest_info_provider: &'a QuestInfoProvider
 }
 
 impl<'a> MapModesQuestCreator<'a>  {
-    pub fn new(qip: &QuestInfoProvider) -> Self {
+    pub fn new(qip: &'a QuestInfoProvider) -> Self {
         MapModesQuestCreator { 
             quest_info_provider: qip
         }
@@ -50,14 +55,46 @@ impl<'a> PatchModifyable for MapModesQuestCreator<'a> {
     type Modifyable = ObjectivesInfo;
 
     fn try_modify(&mut self, object: &mut Self::Modifyable) {
-        match object.primary.common.unwrap().objectives {
-            Some(mut objectives) => {
-                objectives.items.unwrap().append(&mut self.quest_info_provider.primary_quests);
+        match object.primary.common.as_mut().unwrap().objectives.as_mut() {
+            Some(objectives) => {
+                for quest in self.quest_info_provider.primary_quests.iter() {
+                    match objectives.items.as_mut() {
+                        Some(items) => items.push(quest.clone()),
+                        None => objectives.items = Some(vec![quest.clone()])
+                    }
+                }
             },
             None => {
-                object.primary.common.unwrap().objectives = Some(Objectives { 
-                    items: Some(self.quest_info_provider.primary_quests) 
+                object.primary.common.as_mut().unwrap().objectives = Some(Objectives { 
+                    items: Some(self.quest_info_provider.primary_quests.clone()) 
                 })
+            }
+        }
+    }
+}
+
+pub struct QuestEmptyItemsFixer {
+}
+
+impl PatchModifyable for QuestEmptyItemsFixer {
+    type Modifyable = ObjectivesInfo;
+
+    fn try_modify(&mut self, object: &mut Self::Modifyable) {
+        if object.primary.common.as_ref().unwrap().objectives.as_ref().unwrap().items.is_none() {
+            object.primary.common.as_mut().unwrap().objectives = None;
+        }
+        for ql in object.primary.player_specific.items.as_mut().unwrap().iter_mut() {
+            if ql.objectives.as_ref().unwrap().items.is_none() {
+                ql.objectives = None;
+            }
+        }
+        //
+        if object.secondary.common.as_ref().unwrap().objectives.as_ref().unwrap().items.is_none() {
+            object.primary.common.as_mut().unwrap().objectives = None;
+        }
+        for ql in object.secondary.player_specific.items.as_mut().unwrap().iter_mut() {
+            if ql.objectives.as_ref().unwrap().items.is_none() {
+                ql.objectives = None;
             }
         }
     }
